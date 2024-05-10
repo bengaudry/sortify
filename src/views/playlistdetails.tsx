6dfbc45b33cd798925d5bfa0d80d12d4ea5e1416
bengaudry <bengaudry@outlook.fr>
2024-05-10 13:36:24 +0200

    Feature: DND

--- src/views/playlistdetails.tsx
+++ src/views/playlistdetails.tsx
@@ -3,6 +3,24 @@ import { CtaLink } from "@/components/cta";
 import { useRouter } from "next/navigation";
 import { useEffect, useRef, useState } from "react";
 import { fetchPlaylistDetails } from "@/api/spotify";
+import {
+  closestCorners,
+  DndContext,
+  KeyboardSensor,
+  PointerSensor,
+  TouchSensor,
+  useSensor,
+  useSensors,
+} from "@dnd-kit/core";
+import {
+  arrayMove,
+  SortableContext,
+  sortableKeyboardCoordinates,
+  useSortable,
+  verticalListSortingStrategy,
+} from "@dnd-kit/sortable";
+import { track } from "@vercel/analytics";
+import { CSS } from "@dnd-kit/utilities";
 
 const formatMsDuration = (duration_ms: number): string => {
   let seconds = Math.floor(duration_ms / 1000);
@@ -84,6 +102,12 @@ export function PlaylistDetailsPage({ listid }: { listid: string }) {
     return () => obs.disconnect();
   }, []);
 
+  const sensors = useSensors(
+    useSensor(PointerSensor),
+    useSensor(TouchSensor),
+    useSensor(KeyboardSensor, { coordinateGetter: sortableKeyboardCoordinates })
+  );
+
   return (
     <div
       className={`pl-4 pr-6 py-12 bg-gradient-to-b from-[${playlist?.primary_color}] to-black`}
@@ -157,30 +181,59 @@ export function PlaylistDetailsPage({ listid }: { listid: string }) {
         <div className="flex-1" />
       </div>
 
-      <div className="flex flex-col gap-4">
-        {tracksItems?.map((track, idx) => (
-          <TrackDisplayer
-            idx={idx}
-            track={track.track}
-            onup={() => {
-              setTracksItems((prev) => {
-                const newarr = prev?.filter((_, i) => i !== idx);
-                newarr?.splice(idx - 1, 0, track);
-                moveSongUp(idx);
-                return newarr;
-              });
-            }}
-            ondown={() => {
-              setTracksItems((prev) => {
-                const newarr = prev?.filter((_, i) => i !== idx);
-                newarr?.splice(idx + 1, 0, track);
-                moveSongDown(idx);
-                return newarr;
-              });
-            }}
-          />
-        ))}
-      </div>
+      {tracksItems && (
+        <DndContext
+          collisionDetection={closestCorners}
+          sensors={sensors}
+          onDragEnd={(e) => {
+            console.log("dragend", e);
+            const { active, over } = e;
+            if (!over || !tracksItems || active.id === over.id) return;
+
+            setTracksItems((items) => {
+              const originalPos = active.id as number;
+              const newPos = over.id ? parseInt(over.id.toString()) : 0;
+
+              return arrayMove(
+                items as PlaylistTrackObject[],
+                originalPos,
+                newPos
+              );
+            });
+          }}
+        >
+          <div className="flex flex-col">
+            <SortableContext
+              items={tracksItems.map((_, index) => index)}
+              strategy={verticalListSortingStrategy}
+            >
+              {tracksItems.map((track, idx) => (
+                <TrackDisplayer
+                  idx={idx}
+                  key={idx}
+                  track={track.track}
+                  onup={() => {
+                    setTracksItems((prev) => {
+                      const newarr = prev?.filter((_, i) => i !== idx);
+                      newarr?.splice(idx - 1, 0, track);
+                      moveSongUp(idx);
+                      return newarr;
+                    });
+                  }}
+                  ondown={() => {
+                    setTracksItems((prev) => {
+                      const newarr = prev?.filter((_, i) => i !== idx);
+                      newarr?.splice(idx + 1, 0, track);
+                      moveSongDown(idx);
+                      return newarr;
+                    });
+                  }}
+                />
+              ))}
+            </SortableContext>
+          </div>
+        </DndContext>
+      )}
     </div>
   );
 }
@@ -196,8 +249,22 @@ function TrackDisplayer({
   onup: () => void;
   ondown: () => void;
 }) {
+  const { attributes, listeners, setNodeRef, transform, transition } =
+    useSortable({ id: idx });
+
+  const style = {
+    transition,
+    transform: CSS.Transform.toString(transform),
+  };
+
   return (
-    <div className="flex flex-row justify-between gap-2">
+    <div
+      ref={setNodeRef}
+      {...attributes}
+      {...listeners}
+      style={style}
+      className="flex flex-row justify-between gap-2 py-2 select-none"
+    >
       <div className="flex flex-row items-center">
         <span className="text-white/50 w-4 text-right mr-2">{idx + 1}</span>
         <div className="flex flex-col">
