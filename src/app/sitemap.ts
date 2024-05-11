7c87c0537c245f3a82cbd35189f0dd8588e0cc97
bengaudry <bengaudry@outlook.fr>
2024-05-11 17:23:59 +0200

    sitemap

---
+++ src/app/sitemap.ts
@@ -0,0 +1,24 @@
+import { MetadataRoute } from "next";
+
+const URL = "https://websortify.vercel.com/";
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  return [
+    {
+      url: URL,
+      lastModified: new Date(),
+      priority: 1,
+    },
+    {
+      url: URL + "playlists",
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.6,
+    },
+    {
+      url: URL + "playlistdetails",
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+    },
+  ];
+}
