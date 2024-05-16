3e9a75eb40259b01d80d0a0038cc2f9b1627d0f6
bengaudry <bengaudry@outlook.fr>
2024-05-16 13:29:13 +0200

    New deployment on branch main

--- src/views/legal.tsx
+++ src/views/legal.tsx
@@ -1,32 +1,41 @@
 "use client";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import legalmd from "../../public/legal";
 import ReactMarkdown from "react-markdown";
 
 export function LegalPage() {
   const [lang, setLang] = useState<"en" | "fr">("en");
 
+  useEffect(() => {
+    if (navigator.language === "fr-FR") {
+      setLang("fr");
+    }
+  }, []);
+
   return (
-    <div className="px-6 py-12">
-      <div className="flex justify-between items-center">
+    <div className="pb-12 pt-8 bg-spotify-800">
+      <div className="flex justify-between items-center sticky h-20 top-0 px-6 bg-spotify-800">
         <a className="flex items-center gap-2" href="/">
           <img src="/icon-rounded.svg" width={30} height={30} />
           <span className="text-spotify-200 font-medium text-xl">Sortify</span>
         </a>
         <select
-          className="bg-spotify-800 px-4 py-2 rounded-sm"
+          className="bg-spotify-700 px-4 py-2 rounded-sm"
           onChange={({ target: { value } }) =>
             setLang(value === "en" || value === "fr" ? value : "en")
           }
         >
-          <option selected value="en">
+          <option selected={lang === "en"} value="en">
             English
           </option>
-          <option value="fr">Français</option>
+          <option selected={lang === "fr"} value="fr">
+            Français
+          </option>
         </select>
       </div>
 
       <ReactMarkdown
+        className="px-12"
         components={{
           a: ({ className, target, ...otherProps }) => (
             <a
@@ -38,7 +47,10 @@ export function LegalPage() {
             />
           ),
           h1: ({ className, ...otherProps }) => (
-            <h1 className="text-4xl font-bold mt-9 mb-3" {...otherProps} />
+            <h1
+              className="block w-screen -ml-12 pl-12 text-4xl font-bold mt-6 py-3 bg-spotify-800/50 sticky top-20 backdrop-blur-md"
+              {...otherProps}
+            />
           ),
           h2: ({ className, ...otherProps }) => (
             <h2 className="text-xl font-semibold mt-6" {...otherProps} />
@@ -53,6 +65,16 @@ export function LegalPage() {
       >
         {lang in legalmd ? legalmd[lang] : legalmd["en"]}
       </ReactMarkdown>
+      <div className="w-full flex justify-end px-12">
+        <button
+          className="bg-spotify-700 aspect-square w-14 rounded-xl"
+          onClick={() => {
+            window.scrollTo({ top: 0, left: 0, behavior: "smooth" });
+          }}
+        >
+          <i className="fi fi-rr-angle-up" />
+        </button>
+      </div>
     </div>
   );
 }
